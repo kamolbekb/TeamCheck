846883ed5a63f614bafce6982ffe5ebc865ea8c8
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2023-08-19 13:33:24 +0500

    Program was successfully updated

---
+++ TeamChek/TeamChek/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Hometask
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		public string Model { get; set; }
+		public string Brand { get; set; }
+		public decimal Price { get; set; }
+		public static decimal TaxPercentage { get; set; } = 10;
+		public Car(int id,string model,string brand,decimal price)
+		{
+			Id = id;
+			Model = model;
+			Brand = brand;
+			Price = price;
+		}
+		public void CalculateTotal()
+		{
+			Console.WriteLine($"Total price of the car including taxes: {Price + ((Price / 100) * TaxPercentage)}");
+		}
+	}
+}
+
--- TeamChek/TeamChek/Program.cs
+++ TeamChek/TeamChek/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-
+            Car car = new Car(23, "Roadster", "Tesla", 15000);
+            Car car1 = new Car(21, "SVJ", "Lamborghini", 10000);
+            Car car2 = new Car(20, "M4 Competition", "BMW", 100000);
+            car.CalculateTotal();
+            car1.CalculateTotal();
+            car2.CalculateTotal();
+            Console.WriteLine("--------changed--------");
+            Car.TaxPercentage = 20;
+            car.CalculateTotal();
+            car1.CalculateTotal();
+            car2.CalculateTotal();
         }
     }
 }
